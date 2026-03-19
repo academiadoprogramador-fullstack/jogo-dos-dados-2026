2cffbede9d5be3efe3e707b89f19674432b0d9c5
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-19 13:59:18 -0300

    Adicionada lógica inicial do jogo

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace JogoDosDados.ConsoleApp;
+﻿using System.Security.Cryptography;
+
+namespace JogoDosDados.ConsoleApp;
 
 /*
 1. Pista:
@@ -25,6 +27,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("-------------------------------------------");
+
+            // Lógica do Jogo
+            Console.Write("Pressione ENTER para lançar um dado...");
+            Console.ReadLine();
+
+            int resultado = RandomNumberGenerator.GetInt32(1, 7);
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine($"O número sorteado foi: {resultado}");
+            Console.WriteLine("-------------------------------------------");
+
+            Console.Write("Deseja continuar? (s/N): ");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
+        }
     }
 }
