8944291dadcc811eea6725f427f4ddee5d8708b3
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-19 14:49:14 -0300

    Adicionado o turno do computador

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -33,6 +33,7 @@ class Program
         while (true)
         {
             int posicaoJogador = 0;
+            int posicaoComputador = 0;
             bool jogoEstaEmAndamento = true;
 
             while (jogoEstaEmAndamento)
@@ -41,18 +42,20 @@ class Program
                 Console.WriteLine("-------------------------------------------");
                 Console.WriteLine("Jogo dos Dados");
                 Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Rodada do Jogador");
+                Console.WriteLine("-------------------------------------------");
 
                 // Lógica do Jogo
                 Console.Write("Pressione ENTER para lançar um dado...");
                 Console.ReadLine();
 
-                int resultado = RandomNumberGenerator.GetInt32(1, 7);
+                int resultadoJogador = RandomNumberGenerator.GetInt32(1, 7);
 
                 Console.WriteLine("-------------------------------------------");
-                Console.WriteLine($"O número sorteado foi: {resultado}");
+                Console.WriteLine($"O número sorteado foi: {resultadoJogador}");
                 Console.WriteLine("-------------------------------------------");
 
-                posicaoJogador += resultado;
+                posicaoJogador += resultadoJogador;
 
                 Console.WriteLine($"Você está na posição: {posicaoJogador} de {limiteLinhaChegada}");
 
@@ -77,10 +80,55 @@ class Program
                     Console.WriteLine("\nParabéns! Você alcançou a linha de chegada.");
 
                     jogoEstaEmAndamento = false;
+
+                    Console.Write("\nPressione ENTER para continuar...");
+                    Console.ReadLine();
+
+                    continue;
                 }
 
-                Console.WriteLine();
-                Console.Write("Pressione ENTER para continuar...");
+                Console.Write("\nPressione ENTER para continuar...");
+                Console.ReadLine();
+
+                // Implementação do turno computador
+                Console.Clear();
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Rodada do Computador");
+                Console.WriteLine("-------------------------------------------");
+
+                int resultadoComputador = RandomNumberGenerator.GetInt32(1, 7);
+
+                Console.WriteLine($"O número sorteado foi: {resultadoComputador}");
+                Console.WriteLine("-------------------------------------------");
+
+                posicaoComputador += resultadoComputador;
+
+                Console.WriteLine($"O computador está na posição: {posicaoComputador} de {limiteLinhaChegada}");
+
+                if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15 || posicaoComputador == 25)
+                {
+                    Console.WriteLine($"\nEVENTO: Avanço de {bonusAvancoExtra} casas!");
+                    posicaoComputador += bonusAvancoExtra;
+
+                    Console.WriteLine($"\nO computador está na posição: {posicaoComputador} de {limiteLinhaChegada}");
+                }
+
+                else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
+                {
+                    Console.WriteLine($"\nEVENTO: Recuo de {penalidadeRecuo} casas");
+                    posicaoComputador -= penalidadeRecuo;
+
+                    Console.WriteLine($"\nO computador está na posição: {posicaoComputador} de {limiteLinhaChegada}");
+                }
+
+                if (posicaoComputador >= limiteLinhaChegada)
+                {
+                    Console.WriteLine("\nQue pena! O computador alcançou a linha de chegada.");
+
+                    jogoEstaEmAndamento = false;
+                }
+
+                Console.Write("\nPressione ENTER para continuar...");
                 Console.ReadLine();
             }
 
